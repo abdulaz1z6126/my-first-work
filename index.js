a2f513766afd989ce7a692dabca9ded41bf84d31
abdulaz1z6126 <toraqulovabdulaziz6126@gmail.com>
2024-04-19 15:49:31 +0500

    m

---
+++ index.js
@@ -0,0 +1,105 @@
+// 1 ga👇
+
+// let minut = 5
+// let second = 60
+
+// function hisob() {
+//     return (minut * second);
+// }
+// console.log("second:",hisob())
+
+// =========================================================================
+// 2 ga👇
+
+// let yosh = 19;
+// let yil = 365;
+
+// function hisob() {
+//     return yosh * yil;
+// }
+
+// console.log("yil:", hisob());
+
+// ========================================================================
+// 3 ga👇
+
+// let arr = [1,2,3,4,5,6,7]
+
+// function hisob() {
+//     return arr
+// }
+
+// console.log(arr.find(hisob))
+// ==========================================================================
+// 4 ga👇
+
+// let a = 5;
+// let b = 2;
+
+// function hisob() {
+//   return a % b;
+// }
+// console.log("qoldiq:", hisob());
+// =========================================================================
+// 5 ga👇
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// let num = 0;
+
+// function hisob() {
+//   for (let i = 0; i < arr.length; i++) {
+//     num += arr[i];
+//   }
+//   return num;
+// }
+// console.log(hisob());
+
+// chunganim shu boldi !!
+
+// ============================================================================
+// 6 ga👇
+
+// let arr = ["a", 1, "bir narsa", 2, "har qanday", 3, 4, 5, "a"]
+// let newArr = []
+
+// function hisob() {
+//     for(let i = 0; i < arr.length; i++){
+//         if(typeof arr[i] === "number"){
+//             newArr.push(arr[i])
+//         }
+//     }
+//     return newArr
+// }
+
+// console.log(hisob())
+// =========================================================================
+// 7 ga👇
+
+// let Number = "5.5";
+// let type = "float";
+
+// function hisob() {
+//   if (type === "float") {
+//     return parseFloat(Number);
+//   }
+// }
+
+// console.log(hisob("5.5", "float"));
+
+// 2 misol
+
+// let Number = "88";
+// let type = "int";
+
+// function hisob() {
+//   if (type === "int") {
+//     return parseInt(Number);
+//   }
+// }
+
+// console.log(hisob("88", "int"));
+
+// ======================================================
+
+// 8 ga kampiyuterim ishlamidi😅
+//cwom3
